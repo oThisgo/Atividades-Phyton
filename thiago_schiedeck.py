6071efc824411a92145e1860fe9070b51c6c2c57
Thiago Schiedeck <103116333+oThisgo@users.noreply.github.com>
2023-06-19 20:26:19 -0300

    Update thiago_schiedeck.py

--- thiago_schiedeck.py
+++ thiago_schiedeck.py
@@ -8,26 +8,34 @@ def menu():
     print("2 - Listar Cadeiras")
     print("3 - Posição das Cadeiras")
     print("4 - Inserir Números")
-    print("4 - Listar Números")
+    print("5 - Listar Números")
+    print("6 - Converter Números")
     print("10 - FIM")
     print("")
     print("Escolha Uma Opção:")
     
 def inserecadeira():
+    print("")
     cadeiras = input("Digite a cadeira: ")
     listaA.append(cadeiras.upper())
+    print("")
     continuar = input("Deseja continuar (S/N)? ")
     while continuar.upper() == "S":
+        print("")
         cadeiras = input("Digite a cadeira: ")
         listaA.append(cadeiras.upper())
+        print("")
         continuar = input("Deseja continuar (S/N)? ")
         
 def listarcadeiras():
+    print("")
     print ("Suas cadeiras são:")
     for i in listaA:
-        print(i)
-
+        print(f"{i[0:3]}")
+    print("")
+    
 def posicaocadeira():
+    print("")
     listaAtamanhos = []
     for i in range(len(listaA)):
         tamanho = len(listaA[i])
@@ -41,17 +49,47 @@ def posicaocadeira():
     posicao0 = listaA.index(cadeira_maiusc)
     posicao1 = listaAtamanhos[posicao0]
     posicao_final = sort_index.index(posicao1)
+    print("")
     print(f"""A cadeira de {cadeira} é a {posicao_final+1}ª com maior número de caracteres.""")
+    print("")
     
 def inserenumeros():
+    print("")
     for i in range (10):
-        numeros = int(input(f"Digite o {i}º número inteiro entre 100 e 500: "))
+        numeros = int(input(f"Digite o {i+1}º número inteiro entre 100 e 500: "))
         if numeros >= 100 and numeros <= 500:
             listaB.append(numeros)
             continue
         else:
+            print("")
             print("Ops, parece que o número que você digitou não está entre 100 e 500. :(")
             break
+        
+def listarnumeros():
+    print("")
+    print("Os números digitados foram: ")
+    for i in listaB:
+        print(i)
+    multiplos = []
+    for x in range (len(listaB)):
+        resultado = listaB[x] % 10
+        if resultado == 0:
+            multiplos.append(listaB[x])
+        else:
+            continue
+    print("")
+    print("Entre estes, terminam em 0:")
+    for y in multiplos:
+        print(y)
+        print("")
+        
+def conversao():
+    print("")
+    indice = int(input("Digite o índice do número que você deseja converter:"))
+    listaB[indice] = str(listaB[indice])
+    print("")
+    print(f"Pronto, o valor {listaB[indice]} foi transformado em uma string.")
+    print("")
         
 while True:
     menu()
@@ -66,4 +104,10 @@ while True:
     elif opcao == 3:
         posicaocadeira()
     elif opcao == 4:
-        inserenumeros()    
+        inserenumeros()  
+    elif opcao == 5:
+        listarnumeros() 
+    elif opcao == 6:
+        conversao() 
+    
+    
