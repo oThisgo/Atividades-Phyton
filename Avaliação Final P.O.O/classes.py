35065d1e8aa44a4112906398bf90697fe38406d8
Thiago Schiedeck <103116333+oThisgo@users.noreply.github.com>
2023-11-23 11:11:36 -0300

    Add files via upload

---
+++ Avaliação Final P.O.O/classes.py
@@ -0,0 +1,9 @@
+class Tarefa:
+    def __init__(self, titulo, descricao, status, criacao, conclusao):
+        self.__titulo = titulo
+        self.__descricao = descricao
+        self.__status = status
+        self.__criacao = criacao
+        self.__conclusao = conclusao
+        
+    def criar_tarefa()
